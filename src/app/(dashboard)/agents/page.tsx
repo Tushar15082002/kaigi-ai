0c66819991ff2f2b020b6130b458afe5d16efaf3
Tushar15082002 <94022900+Tushar15082002@users.noreply.github.com>
2025-09-02 23:37:21 +0530

    Added Agents Form

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,19 +1,39 @@
-import { AgentsView, AgentsViewError, AgentsViewLoading } from "@/modules/agents/ui/views/agents-view";
-import { getQueryClient, trpc } from "@/trpc/server";
-import { ErrorBoundary } from "react-error-boundary";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Suspense } from "react";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { auth } from "@/lib/auth";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
+import { 
+    AgentsView, 
+    AgentsViewError, 
+    AgentsViewLoading 
+} from "@/modules/agents/ui/views/agents-view";
 
 const Page = async () => {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+    
+    if(!session) {
+        redirect("/sign-in")
+    }
+
     const queryClient = getQueryClient();
     void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
     return (
-        <HydrationBoundary state={dehydrate(queryClient)}>
-            <Suspense fallback={<AgentsViewLoading />} />
-            <ErrorBoundary fallback={<AgentsViewError />}>
-                <AgentsView />
-            </ErrorBoundary>
-        </HydrationBoundary>
+        <>
+            <AgentsListHeader />
+            <HydrationBoundary state={dehydrate(queryClient)}>
+                <Suspense fallback={<AgentsViewLoading />} />
+                <ErrorBoundary fallback={<AgentsViewError />}>
+                    <AgentsView />
+                </ErrorBoundary>
+            </HydrationBoundary>
+        </>
     )
 };
 
